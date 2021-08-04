837f2129a900f61c629204b02e9d2f5d9ef1c89d
Blessing Agadagba <agadagbablessing@gmail.com>
2021-08-04 22:07:48 +0300

    added source code

---
+++ src/database.py
@@ -0,0 +1,111 @@
+ 
+import psycopg2
+import pandas as pd
+import csv
+from sqlalchemy import create_engine, types
+class DatabaseError(psycopg2.Error):
+    pass
+class DatabaseConnector():
+    def __init__(self):
+        self.__connection = None
+        self.__cursor = None
+
+    def connect_database(self):
+        """
+        creates a connection to the postgres database.
+        :return: connection cursor
+        """
+        try:
+            self.__connection = psycopg2.connect(
+                host="ec2-79-125-30-28.eu-west-1.compute.amazonaws.com",
+                user="uxqtimqdzddpcq",
+                port="5432",
+                database="d6coulm169a0sf",
+                password="ec463706ffd1e095dc7639ab562cb1d783e3c8c8d9bb6f0227b9f286384a7ace"
+            )
+            self.__connection.autocommit = True
+            self.__cursor = self.__connection.cursor()
+            return self.__cursor
+        except DatabaseError:
+            raise DatabaseError("There was a problem connecting to the requested database.")
+
+
+    def create_tables(self):
+        """
+        Create tables and insert dataframe in database.
+        :return: None
+        """
+
+        cur = self.connect_database()
+        try:
+            cur.execute('''
+                CREATE TABLE IF NOT EXISTS categories (
+                    id serial PRIMARY KEY,
+                    category VARCHAR(50)
+                );
+                ''')
+
+            cur.execute('''
+                CREATE TABLE IF NOT EXISTS booksdepo (
+                id serial PRIMARY KEY,
+                book_title varchar(10000),
+                book_price varchar(255),
+                book_url varchar(10000),
+                image_url varchar(10000),
+                Published_date date,
+                category varchar(255)
+                );
+                ''')
+
+        except (DatabaseError, Exception):
+            raise DatabaseError("Could not create tables in the specified database")
+        
+    def delete_tables(self):
+        """
+        deletes Listing and Categories tables from the database
+        :return: Tables successfully deleted message
+        """
+        cur = self.connect_database()
+        try:
+            cur.execute("DROP TABLE IF EXISTS booksdepo CASCADE ")
+            cur.execute("DROP TABLE IF EXISTS categories CASCADE")
+            print("booksdepo and Categories tables no longer in database.")
+        except (Exception, DatabaseError):
+            raise DatabaseError("Could not drop tables in the specified database")    
+        
+        
+    def insert_into_tables(self,path):
+        """
+        inserts Listing and Categories tables from the database
+        :return: Tables successfully inserted message
+        """
+        try:
+            cur = self.connect_database()  
+            df = pd.read_csv(path)
+            data = df.to_records(index=False)
+            host="ec2-79-125-30-28.eu-west-1.compute.amazonaws.com"
+            user="uxqtimqdzddpcq"
+            port="5432"
+            database="d6coulm169a0sf"
+            password="ec463706ffd1e095dc7639ab562cb1d783e3c8c8d9bb6f0227b9f286384a7ace"
+            postgres_str = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+    
+            engine = create_engine(postgres_str, echo=False)
+            df.to_sql('booksdepo',con=engine,index=False,if_exists='append')
+            
+            cur.execute('''
+            INSERT INTO categories (category) VALUES('finace');
+            INSERT INTO categories (category) VALUES('chidren');
+            INSERT INTO categories (category) VALUES('economics');
+            ''')
+            print("Succesfully added values to tables")        
+        except (Exception, DatabaseError):
+            raise DatabaseError("Could not add value to  tables in the specified database")    
+        
+        self.__connection.commit()
+if __name__ == '__main__':
+    db = DatabaseConnector()
+    db.connect_database()
+    db.create_tables()
+    path = str(input("Enter the path to the csv:\n"))
+    db.insert_into_tables(path)
---
+++ src/helper.py
@@ -0,0 +1,128 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import math
+from typing import Union
+import pandas as pd
+import re
+import datetime
+import time
+from random import randint
+from dateutil import parser
+
+user_agent = UserAgent()
+headers = {"User-Agent": user_agent.random}
+
+def count_pages(number_of_items: Union[int,float], keyword=None )-> int:  
+    if number_of_items> find_max_item(keyword):
+        no_of_pages = math.ceil(find_max_item(keyword) / 30)
+    no_of_pages = math.ceil(abs(number_of_items) / 30)
+    return no_of_pages
+
+def find_max_item(keyword: str):
+    '''Finds the maximal number of records  that can be scraped given the arguments for a particular keyword 
+    Returns the  max number of items of the category  that can be scrapped from the website
+    '''
+    url = f"https://www.bookdepository.com/search?searchTerm={keyword}&page=1"
+    page = requests.get (url, headers =headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    max_soup = soup.find("div", class_ = "search-info")
+    max_item =  int([number.text for number in max_soup.find_all("span", class_="search-count")][0].replace(',',''))
+    return max_item
+
+def generate_urls(keyword:str, number_of_items:int)-> list:
+    """ """
+    urls = []
+    if  number_of_items >= find_max_item(keyword):
+        number_of_pages = count_pages(find_max_item(keyword))
+        
+        for page_number in range(1,number_of_pages+1):
+            url = f"https://www.bookdepository.com/search?searchTerm={keyword}&page={page_number}"
+            urls.append(url)       
+    else:
+        number_of_pages = count_pages(number_of_items)
+        for page_number in range(1,number_of_pages+1):
+            url = f"https://www.bookdepository.com/search?searchTerm={keyword}&page={page_number}"
+            urls.append(url)
+    return urls   
+   
+def extract(url):
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    book_items = soup.find_all("div", class_="book-item")
+    
+    urls_to_image = []
+    urls_to_item = []
+    date_published = []
+    titles = []
+    prices = []
+    
+    
+    for  item in book_items:
+        image = item.find("div", class_="item-img")
+        image_url =  image and image.find("img", class_="lazy").get("data-lazy") or ''
+        urls_to_image.append(image_url)
+        date = item.find("p", class_="published")
+        date =  date  and date.text.strip() or ''
+        date_published.append(date)
+        title = item.find("h3", class_="title")
+        title =  title and title.text.strip() or ''
+        titles.append(title)
+        url_to_item = item.select("h3.title a[href]")
+        url_to_item = url_to_item and "https://www.bookdepository.com"+ url_to_item[0]["href"] or ''
+        urls_to_item.append(url_to_item)
+        price = item.find("p", class_="price")
+        price = price and price.text.strip()[0:8] or '0'
+        prices.append(price)
+      
+    dict_of_scrapped_items = {
+        'book_title':titles,
+        'book_price':prices,
+        'book_url':urls_to_item,
+        'image_url':urls_to_image,
+        'published_date':date_published
+    }
+    return  dict_of_scrapped_items
+
+
+def transform(df):
+    try:
+        df['book_price'] = df['book_price'].apply(lambda x :re.sub("[^0-9 ,.]", "",x).replace(",", "."))
+        df['Published_date'] = df['Published_date'].apply(lambda x: parser.parse(x) )
+        df['category'] = keyword
+    except:
+        df['book_price'] = df['book_price']
+        df['published_date'] = df['published_date']
+       
+    return df 
+
+  
+if __name__ == '__main__':
+    keyword = str(input('what category of books do you want to scrape?\n'))
+    number_of_items = int(input("how many items do you want to scrape?\n"))
+    result = []
+    urls = generate_urls(keyword, number_of_items)
+
+    data = {
+        'book_title':[],
+        'book_price': [],
+        'book_url':[],
+        'image_url':[],
+        'published_date':[]
+    }
+    page_number = 1
+    for url in urls:
+       
+        print(f"**********************Scraping page {page_number}***************")
+        data_dict = extract(url)
+        data['book_title'].extend(data_dict['book_title'])
+        data['book_price'].extend(data_dict['book_price'])
+        data['book_url'].extend(data_dict['book_url'])
+        data['image_url'].extend(data_dict['image_url'])
+        data['published_date'].extend(data_dict['published_date'])
+        time.sleep(randint(1, 5))
+        page_number += 1
+    df = pd.DataFrame(data)     
+    df = transform(df)
+    df['category'] = keyword
+    df.to_csv('df.csv', index=False)
